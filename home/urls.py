6cda28ef144520c48c0110f0861b3c9227788a24
omidima <o.h.hadidy@gmail.com>
2022-03-18 15:08:11 +0330

    add review and optimize resource retrive media

--- home/urls.py
+++ home/urls.py
@@ -1,15 +1,24 @@
 from django.urls import path, include
-from .views import IndexUserList ,getInstagramPosts, InstagramPost, ResourceItems, IndexPostList,PostDetail
-from rest_framework import routers
+from .views import IndexUserList, GetInstagramPosts, InstagramPost, ResourceItems, IndexPostList, PostDetail
+from . import views
+from rest_framework_nested import routers
 
 route = routers.DefaultRouter()
-route.register('posts',InstagramPost)
-route.register('resource',ResourceItems)
+route.register('posts', InstagramPost)
+route.register('resource', ResourceItems)
+
+review = routers.NestedDefaultRouter(route, 'posts', lookup='posts')
+review.register('reviews', views.ReviewViewModel, basename='posts-reviews')
+
+resource = routers.NestedDefaultRouter(route, 'resource', lookup='resources')
+resource.register('media', views.GetResourceMedia, basename='media-resource')
 
 urlpatterns = [
-    path('get-posts',getInstagramPosts.as_view()),
+    path('get-posts',GetInstagramPosts.as_view()),
     path('',IndexUserList.as_view()),
     path('posts',IndexPostList.as_view()),
     path('posts/<int:pk>',PostDetail.as_view()),
+    path('api/',include(review.urls)),
+    path('api/',include(resource.urls)),
     path('api/', include(route.urls))
 ]
